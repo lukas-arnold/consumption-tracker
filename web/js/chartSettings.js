c944114fe80fb3b2f2192413b56e4d1c2d9e42ae
Lukas Arnold <ow1hbj68@arnold-lukas.de>
2026-06-06 10:41:15 +0200

    refactor design

--- web/js/chartSettings.js
+++ web/js/chartSettings.js
@@ -1,1 +1,8 @@
 Chart.defaults.elements.line.tension = 0.4;
+// Chart.defaults.responsive = true;
+// Chart.defaults.maintainAspectRatio = true;
+
+Chart.defaults.interaction = {
+    mode: 'index',
+    intersect: false
+};
